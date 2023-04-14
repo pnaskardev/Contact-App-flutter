ebc4a38a070b6afd9050f2882c538891e8cc750d
pnaskardev <priyanshunaskar89@gmail.com>
2023-04-14 12:24:39 +0530

    added user model

---
+++ lib/features/auth/services/auth_service.dart
@@ -0,0 +1,21 @@
+class AuthService
+{
+  // sign up user
+  void signUpUser
+  ({
+    required String name,
+    required String email,
+    required String phone,
+    required String password,
+  }) async
+  {
+    try 
+    {
+      
+    } 
+    catch (e) 
+    {
+      
+    }
+  }
+}
---
+++ lib/model/user.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User _$UserFromJson(Map<String, dynamic> json) => User(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      phone: json['phone'] as String,
+      email: json['email'] as String,
+      password: json['password'] as String,
+      token: json['token'] as String,
+    );
+
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'phone': instance.phone,
+      'email': instance.email,
+      'password': instance.password,
+      'token': instance.token,
+    };
